91cbeb0cffa40c83c17797a37db755b5d0e5fa9e
Sham Sundar <shamparmar.seo@gmail.com>
2024-05-01 19:57:16 +0500

    Written 1 method in model controller.

--- Back-End/src/controllers/order.controller.js
+++ Back-End/src/controllers/order.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { Order } from "../models/order.model.js";
 
 const createOrder = asyncHandler(async(req, res)=>{
-    const {totalPrice, } = req.body
+    const {totalPrice, items} = req.body
 })
 
 export {createOrder}
