0b43bad68b22562b07fa1c5f8c1d2bd82f5b2d49
Sham Sundar <shamparmar.seo@gmail.com>
2024-05-01 23:34:10 +0500

    .

--- Back-End/src/controllers/order.controller.js
+++ Back-End/src/controllers/order.controller.js
@@ -2,7 +2,6 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { Order } from "../models/order.model.js";
 
 const createOrder = asyncHandler(async(req, res)=>{
-    const {totalPrice, items} = req.body
 })
 
 export {createOrder}
