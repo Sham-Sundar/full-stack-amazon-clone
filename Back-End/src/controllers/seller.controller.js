7cf843387dd3d3c596d11aec0b88b55ee657024b
Sham Sundar <shamparmar.seo@gmail.com>
2024-05-02 20:30:03 +0500

    .

--- Back-End/src/controllers/seller.controller.js
+++ Back-End/src/controllers/seller.controller.js
@@ -61,7 +61,9 @@ const loginSeller = asyncHandler(async (req, res) => {
 
     const options = {
         httpOnly: true,
-        secure: true
+        secure: true,
+        maxAge: 3600*1000,
+        sameSite: 'strict'
     }
 
     return res
