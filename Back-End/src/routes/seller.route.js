034a50490e1ac8237bf366b25282869e46c1f40c
Sham Sundar <shamparmar.seo@gmail.com>
2024-05-02 10:00:41 +0500

    .

--- Back-End/src/routes/seller.route.js
+++ Back-End/src/routes/seller.route.js
@@ -9,7 +9,7 @@ sellerRouter.route("/register").post(registerSeller)
 sellerRouter.route("/login").post(loginSeller)
 
 // Secured routes
-sellerRouter.route("/logout").get(verifyJWT, logoutSeller)
+sellerRouter.route("/logout").post(verifyJWT, logoutSeller)
 sellerRouter.route("/update-access-token").post(updateAccessToken)
 sellerRouter.route("/update-password").patch(verifyJWT, updatePassword)
 sellerRouter.route("/delete-account").delete(verifyJWT, deleteSellerAccount)
