e243a0fe98a63c77df43cc2e5507794c1140c43f
Sham Sundar <shamparmar.seo@gmail.com>
2024-05-06 02:42:13 +0500

    .

--- Back-End/src/middlewares/auth.middleware.js
+++ Back-End/src/middlewares/auth.middleware.js
@@ -6,7 +6,7 @@ import { Seller } from "../models/seller.model.js";
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
     try {
-        const token = req.header("Authorization")?.replace("Bearer ", "");
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
 
         if (token === "") {
             throw new ApiError(401, "Token is Empty");
